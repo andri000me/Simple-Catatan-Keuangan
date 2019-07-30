8790aa7dd4cc5167e9c10c13ce8c18db8402bf95
Arinadi Nur Rohmad <arinadi.nur@gmail.com>
2019-07-30 12:17:30 +0700

    database

--- keuangan_db.sql
+++ keuangan_db.sql
@@ -1,5 +1,5 @@
 /*
-SQLyog Ultimate v13.1.1 (64 bit)
+SQLyog Ultimate v12.4.3 (64 bit)
 MySQL - 5.7.26-0ubuntu0.18.04.1 : Database - keuangan_test
 *********************************************************************
 */
@@ -23,10 +23,17 @@ CREATE TABLE `category` (
   `name` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `type` enum('in','out') NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 /*Data for the table `category` */
+
+insert  into `category`(`category_id`,`name`,`description`,`type`,`status`) values 
+(4,'test',NULL,'in','inactive'),
+(5,'0ktest','test','out','inactive'),
+(6,'Test','test','out','active'),
+(7,'0ktest','test','in','active');
 
 /*Table structure for table `transaction` */
 
@@ -38,10 +45,16 @@ CREATE TABLE `transaction` (
   `create_date` datetime NOT NULL,
   `nominal` float NOT NULL,
   `description` varchar(255) DEFAULT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
   PRIMARY KEY (`transaction_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `transaction` */
+
+insert  into `transaction`(`transaction_id`,`category_id`,`create_date`,`nominal`,`description`,`status`) values 
+(1,6,'0000-00-00 00:00:00',0,'','inactive'),
+(2,7,'2019-07-30 11:03:22',100000,'test','active'),
+(3,6,'2019-07-30 11:09:38',1000,'keluar','active');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
